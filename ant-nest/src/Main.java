52b7a94b6817d8b0da0a1f9f8f69f330ed95331f
szovi <szovaty@gmail.com>
2025-04-23 17:24:57 +0200

    dev (pheromon init, ants and nutriotion).

--- ant-nest/src/Main.java
+++ ant-nest/src/Main.java
@@ -1,32 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
     private static Nest nest;
 
-    private static Integer size;
-
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) {
         init();
-        for (int i = 0; i < nest.getNestFields().length; i++) {
-            for (int j = 0; j < nest.getNestFields()[i].length; j++) {
-                System.out.print(nest.getNestFields()[i][j].toString());
-            }
-            System.out.println();
-        }
+        System.out.println(nest.toString());
     }
 
     private static void init() {
         nest = new Nest();
     }
-
-
 }
--- ant-nest/src/Nest.java
+++ ant-nest/src/Nest.java
@@ -1,20 +1,31 @@
+import java.util.Random;
+
 public class Nest {
 
     private int size;
+    private Random random;
+
+    private static final int DEFAULT_SIZE = 30;
+    private static final int DEFAULT_ANTS = 10;
+    private static final int DEFAULT_NUTRITION = 10;
+    private static final int DEFAULT_PHEROMONE = 10;
 
     private NestField[][] nestFields;
 
-
     public Nest() {
         this.size = 30;
+        this.random = new Random();
         initNest(this.size);
+        placeInitialElements();
     }
 
     public Nest(Integer n) {
         if (n == null) {
             this.size = 30;
         }
+        this.random = new Random();
         initNest(this.size);
+        placeInitialElements();
     }
 
     private void initNest(int size) {
@@ -22,15 +33,41 @@ public class Nest {
         this.nestFields = new NestField[size + 2][size + 2];
         for (int i = 0; i < nestFields.length; i++) {
             for (int j = 0; j < nestFields[i].length; j++) {
-
                 if (i == 0 || i == nestFields.length - 1 || j == 0 || j == nestFields[i].length - 1) {
                     nestFields[i][j] = new Wall();
                     continue;
                 }
-
                 nestFields[i][j] = new Empty();
             }
         }
+    }
+
+    private void placeInitialElements() {
+        // Place initial ants
+        for (int i = 0; i < DEFAULT_ANTS; i++) {
+            placeRandomElement(new Ant());
+        }
+
+        // Place initial nutrition
+        for (int i = 0; i < DEFAULT_NUTRITION; i++) {
+            placeRandomElement(new Nutrition());
+        }
+
+        // Place initial pheromones
+        for (int i = 0; i < DEFAULT_PHEROMONE; i++) {
+            placeRandomElement(new Pheromone());
+        }
+    }
+
+    private void placeRandomElement(NestField element) {
+        int row, col;
+        do {
+            // +1 because of walls, -2 to avoid walls
+            row = 1 + random.nextInt(nestFields.length - 2);
+            col = 1 + random.nextInt(nestFields.length - 2);
+        } while (!(nestFields[row][col] instanceof Empty));
+
+        nestFields[row][col] = element;
     }
 
     public NestField[][] getNestFields() {
@@ -43,8 +80,13 @@ public class Nest {
 
     @Override
     public String toString() {
-        String display = "";
-
-        return display;
+        StringBuilder display = new StringBuilder();
+        for (NestField[] row : nestFields) {
+            for (NestField field : row) {
+                display.append(field.toString());
+            }
+            display.append("\n");
+        }
+        return display.toString();
     }
 }
